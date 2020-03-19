a7aacab3b1d4ac74841a2ceb3a5b3f503c8a0c76
Eva Rozsa <Eva Rozsa>
2020-03-18 23:43:28 -0600

    done

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let player = {
   name: "DWIGHT",
   choice: [
     {
-      img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fimages%2Fanimals%2Fbear&psig=AOvVaw3aZQnZOlbsoPAZOday8chv&ust=1584655338050000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMi5p6mDpegCFQAAAAAdAAAAABAD",
+      img: "https://gastoniagrizzlies.com/wp-content/uploads/2017/02/Bear-With-Funny-Face-Expression.jpg",
       status: "bears"
     },
     {
@@ -10,27 +10,49 @@ let player = {
       status: "beats"
     },
     {
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS7wdRInRcJt7LHcV4XVaVTXJ-R22wm8oFTW4JzQ6baRvMUGB9e",
+      img: "https://i.pinimg.com/originals/45/f4/3b/45f43b05b4c26ee7d7ea70a94a8fd57f.jpg",
       status: "battlestar"
     }
   ]
 }
 
+
+
 function playBackRandom() {
   let choiceIndex = Math.floor(Math.random() * player.choice.length)
   let choice = player.choice[choiceIndex]
+  document.getElementById('dwightImg').innerHTML = `<img class="img-fluid pl-5 pt-5" src="${choice.img}">`
   return choice
 }
 function play(choice) {
   let Dwight = playBackRandom()
   console.log(Dwight);
-
-
+  if (choice == "bears") {
+    document.getElementById('jimImg').innerHTML = `<img class="img-fluid pl-5 pt-5"
+      src="https://gastoniagrizzlies.com/wp-content/uploads/2017/02/Bear-With-Funny-Face-Expression.jpg"
+      alt="">`
+  }
+  else if (choice == "beats") {
+    document.getElementById('jimImg').innerHTML = `<img class="img-fluid pl-5 pt-5"
+      src="https://pbs.twimg.com/profile_images/490245981481336832/XDWbz6Zb.jpeg"
+      alt="">`
+  }
+  else if (choice == "battlestar") {
+    document.getElementById('jimImg').innerHTML = `<img class="img-fluid pl-5 pt-5"
+      src="https://i.pinimg.com/originals/45/f4/3b/45f43b05b4c26ee7d7ea70a94a8fd57f.jpg"
+      alt="">`
+  }
   if (choice == Dwight.status) {
-    return console.log("draw");
+    // return console.log("draw");
+    document.getElementById('conclusion').innerHTML = `<h1>DRAW</h1>`
   }
-  if (choice == "bears" && Dwight.status == "battlestar" || choice == "beats" && Dwight.status == "bears" || choice == "battlestar" && Dwight.status == "beats") {
-    return console.log("you win");
+  else if (choice == "bears" && Dwight.status == "battlestar" || choice == "beats" && Dwight.status == "bears" || choice == "battlestar" && Dwight.status == "beats") {
+    // return console.log("you win");
+    document.getElementById('conclusion').innerHTML = `<h1>JIM WINS</h1>`
   }
-  return console.log("you loose");
+  else {
+    // return console.log("you loose");
+    document.getElementById('conclusion').innerHTML = `<h1>DWIGHT WINS</h1>`
+  }
 }
+
